1d0030a387bfcd2f16087803d29e13c55fbc3265
Makoto Tokunaga <vios-fish@users.noreply.github.com>
2023-11-30 12:14:16 +0900

    feat(webauto-ci): use custom build spec (#434)

---
+++ .webauto-ci/mot/ecu-system-setup/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+: "${WEBAUTO_CI_GITHUB_TOKEN:?is not set}"
+
+: "${ECU_SYSTEM_SETUP_ANSIBLE_PLAYBOOK:?is not set}"
+
+apt-get -y update
+apt-get -y install "linux-image-$(uname -r)" "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+apt-get -y install ubuntu-minimal openssh-server fonts-ubuntu systemd-coredump vim grub-efi-amd64
+apt-get -y install ubuntu-desktop-minimal --no-install-recommends
+
+# Disable auto suspend
+sed -i 's/\(.*sleep-inactive-ac-timeout=.*\)/sleep-inactive-ac-timeout=0/g' /etc/gdm3/greeter.dconf-defaults
+sed -i 's/\(.*sleep-inactive-battery-timeout=.*\)/sleep-inactive-battery-timeout=0/g' /etc/gdm3/greeter.dconf-defaults
+
+export GITHUB_TOKEN="$WEBAUTO_CI_GITHUB_TOKEN"
+git config --global --add url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+git config --global --add url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "git@github.com:"
+
+eval ansible-playbook "'${ECU_SYSTEM_SETUP_ANSIBLE_PLAYBOOK}'" \
+    -e github_token="${GITHUB_TOKEN}" \
+    -e reload_systemd=no
+
+git config --global --unset-all url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf
+
+mkdir -p /etc/ota
+cp "$(dirname "$0")/persistents.txt" /etc/ota/
+
+sed -i '/^autoware\sALL=(ALL)\sNOPASSWD:ALL/d' /etc/sudoers
