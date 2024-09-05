aa7874e1b6a7a09f884cd844a3262776316b5a12
tkhmy <tkh.my.p@gmail.com>
2024-09-05 17:31:55 +0900

    add kernal install
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- .webauto-ci/signage/ecu-system-setup/run.sh
+++ .webauto-ci/signage/ecu-system-setup/run.sh
@@ -5,6 +5,16 @@
 
 : "${ECU_SYSTEM_SETUP_ANSIBLE_PLAYBOOK:?is not set}"
 
+sudo -E apt-get -y update
+sudo -E apt-get -y install "linux-image-$(uname -r)" "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+sudo -E apt-get -y install systemd udev kmod
+sudo -E apt-get -y install ubuntu-minimal openssh-server fonts-ubuntu systemd-coredump vim grub-efi-amd64
+sudo -E apt-get -y install ubuntu-desktop-minimal --no-install-recommends
+
+# Disable auto suspend
+sudo sed -i 's/\(.*sleep-inactive-ac-timeout=.*\)/sleep-inactive-ac-timeout=0/g' /etc/gdm3/greeter.dconf-defaults
+sudo sed -i 's/\(.*sleep-inactive-battery-timeout=.*\)/sleep-inactive-battery-timeout=0/g' /etc/gdm3/greeter.dconf-defaults
+
 export GITHUB_TOKEN="$WEBAUTO_CI_GITHUB_TOKEN"
 git config --global --add url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 git config --global --add url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/".insteadOf "git@github.com:"
