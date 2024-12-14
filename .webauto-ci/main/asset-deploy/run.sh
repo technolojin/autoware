c3d3395e49f1d14bd21e32e52ed412cb29bdd8df
Masato Saeki <78376491+MasatoSaeki@users.noreply.github.com>
2024-12-14 14:05:51 +0900

    revert(asset-deploy): enable replacement of autoware_data artifacts (#1054)
    Revert "feat(asset-deploy): enable replacement of autoware_data artifacts (#979)"
    
    This reverts commit 899ce4949f1e0052a7f18ef9901d388f04440a51.

--- .webauto-ci/main/asset-deploy/run.sh
+++ .webauto-ci/main/asset-deploy/run.sh
@@ -5,16 +5,11 @@
 : "${ML_MODELS_PATH:=}"
 
 if [ -n "$WEBAUTO_CI_ML_PACKAGES_PATH" ] && [ -n "$ML_MODELS_PATH" ]; then
-    # Ensure ML_MODELS_PATH exists and is owned by the current user
-    sudo mkdir -p "$ML_MODELS_PATH"
-    sudo chown -R "$(id -u):$(id -g)" "$ML_MODELS_PATH"
-
-    # Copy files to target path
-    src_path="${WEBAUTO_CI_ML_PACKAGES_PATH%/}" # Remove trailing slash if present
-    cp -R "${src_path}/." "$ML_MODELS_PATH/"
-
-    find "$ML_MODELS_PATH" -type d -exec chmod 755 {} +
-    find "$ML_MODELS_PATH" -type f -exec chmod 644 {} +
+    sudo mkdir -p "$(dirname "$ML_MODELS_PATH")"
+    sudo chown "$(whoami):" "$(dirname "$ML_MODELS_PATH")"
+    cp -r "$WEBAUTO_CI_ML_PACKAGES_PATH" "$ML_MODELS_PATH"
+    find "$ML_MODELS_PATH" -type d -exec chmod 777 {} \;
+    find "$ML_MODELS_PATH" -type f -exec chmod 644 {} \;
 
     echo "The following ML models have been deployed:"
     ls "$ML_MODELS_PATH"
