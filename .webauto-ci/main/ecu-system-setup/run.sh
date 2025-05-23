aea1991fe3724abd06c6ef3dc3de1e21e14b98c6
takeshi iwa <take.iwiw2222@gmail.com>
2025-05-23 20:47:49 +0900

    feat(webauto-ci): revert removing JetPack "Dev" Packages and "Nsight" for Main ECU (#1856)
    chore: revert ecu-clean-up for main for local build
    
    Signed-off-by: takeshi.iwanari <take.iwiw2222@gmail.com>
    Co-authored-by: Tomohito ANDO <tomohito.ando@tier4.jp>

--- .webauto-ci/main/ecu-system-setup/run.sh
+++ .webauto-ci/main/ecu-system-setup/run.sh
@@ -42,6 +42,6 @@ sudo cp "$(dirname "$0")/persistents.txt" /etc/ota/
 sudo cp "$(dirname "$0")/ignore.txt" /etc/ota/
 
 # clean up ecu firmware
-. .webauto-ci/common/ecu-clean-up/run.sh
+# . .webauto-ci/common/ecu-clean-up/run.sh
 
 sudo sed -i '/^autoware\sALL=(ALL)\sNOPASSWD:ALL/d' /etc/sudoers
