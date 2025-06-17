e1a65d3ff2c7e18d9885846bfb4e7b8c07d8b3f1
Zhenfeng-Sun <zhenfeng.sun@tier4.jp>
2025-06-17 11:30:29 +0900

    feat(webauto-ci): reduce .git folder size and other unnecessary folders (#1953)
    Merge this PR

--- .webauto-ci/common/ecu-clean-up/run.sh
+++ .webauto-ci/common/ecu-clean-up/run.sh
@@ -23,6 +23,7 @@ dev_packages=(
     "libcurand-dev"
     "libcusolver-dev"
     "libcusparse-dev"
+    "libnpp-dev"
     "libnvinfer-dev"
     "libnvinfer-plugin-dev"
     "libnvonnxparsers-dev"
@@ -176,10 +177,45 @@ for package_name in "${ubuntu_desktop_packages[@]}"; do
 done
 echo "----- Finished removing Ubuntu Desktop Packages ..."
 
-echo "----- Start to remove /root/pcl folder ..."
-sudo rm -rf /root/pcl
-echo "----- Finished removing /root/pcl folder ..."
+echo "----- Start to remove unnecessary folders and files ..."
+items_to_remove=(
+    "/boot/Image.backup"
+    "/opt/ros/humble/log"
+    "/opt/autoware/tier4-camera-drivers-for-anvil.tar.gz"
+    "/root/.cache/pip"
+    "/root/pcl"
+    "/root/range-v3"
+)
 
-echo "----- Start to remove /root/range-v3 folder ..."
-sudo rm -rf /root/range-v3
-echo "----- Finished removing /root/range-v3 folder ..."
+for item in "${items_to_remove[@]}"; do
+    if [ -e "$item" ]; then
+        echo "----- Removing: $item"
+        sudo rm -rf "$item"
+    else
+        echo "----- Did not find: $item"
+    fi
+done
+echo "----- Finished removing  unnecessary folders and files  ..."
+
+echo "----- Start to reduce .git folder size ..."
+INITIAL_DIR=$(pwd)
+echo "----- current folder is $INITIAL_DIR"
+find /home/autoware -type d -name ".git" -print0 | while IFS= read -r -d $'\0' git_dir; do
+    # Get the parent directory of the .git folder, which is the actual repository root
+    repo_root=$(dirname "$git_dir")
+    echo "--- Processing Repository: ${repo_root#./} ---"
+    (
+        # Change to the repository root directory
+        cd "$repo_root" || {
+            echo "Error: Could not change to directory '$repo_root'. Skipping."
+            exit 1
+        }
+        echo "Running 'git gc --aggressive --prune=now'..."
+        # Run the git gc command
+        sudo git gc --aggressive --prune=now
+    )
+    echo ""
+done
+
+cd "$INITIAL_DIR"
+echo "----- finished reduce .git folder size ..."
