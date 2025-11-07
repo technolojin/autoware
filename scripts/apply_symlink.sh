6b8cb91d6f9e9f2a9392533d5facf8c4413d2b2f
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2025-11-07 20:10:42 +0900

    fix(apply_symlink.sh): make it work in actual environment (#1930)
    * add comment
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * make it work in actual environment
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * style(pre-commit): autofix
    
    * refactor
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    Co-authored-by: KYabuuchi <24854875+KYabuuchi@users.noreply.github.com>

--- scripts/apply_symlink.sh
+++ scripts/apply_symlink.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# Purpose:
+# This script creates symbolic links from source package files to the `install/share` directories,
+# allowing updates to configuration and launch files without rebuilding the ROS workspace.
+#
+# Background:
+# When using `colcon build --merge-install`, it is necessary to rebuild the workspace every time
+# a launch or configuration file is modified. This process is time-consuming and prone to human error
+# such as forgetting to rebuild. In addition, Git cannot track changes made in the
+# `install/share` directories.
+#
+# Usage:
+# ./apply_symlink.sh
+#
+# Description:
+# - Replaces files in `install/share/<package>/{launch,config}/` with symbolic links
+#   pointing to the original source files
+# - Processes only the packages specified in `REPLACE_PKG_LIST`
+# - Processes only the directories specified in `TARGET_DIRS`
+
+TARGET_DIRS=("launch" "config")
+REPLACE_PKG_LIST=("individual_params" "autoware_launch")
 
 SCRIPT_DIR=$(
     cd "$(dirname "$0")" || exit
@@ -9,9 +30,6 @@ cd "$SCRIPT_DIR" || exit
 cd ../
 
 install_share=./install/share
-TARGET_DIRS=("launch" "config")
-
-REPLACE_PKG_LIST=("individual_params" "autoware_launch")
 
 declare -A pkg_map # name → path
 
@@ -41,9 +59,16 @@ for pkg in "${!pkg_map[@]}"; do
                 rm -rf "${install_pkg_dir:?}/${dir:?}"/*
                 find "$rel_path/$dir" -type f | while read -r src_file; do
                     original_file_path="$(realpath "$src_file")"
-                    file_name="$(basename "$original_file_path")"
-                    # echo $original_file_path $file_name
-                    ln -sf "$original_file_path" "$install_pkg_dir/$dir/$file_name"
+
+                    # get relative path of src_file with respect to rel_path
+                    rel_subpath="${src_file#"${rel_path}"/}"
+                    dest_path="$install_pkg_dir/$rel_subpath"
+
+                    # create the same directory structure
+                    mkdir -p "$(dirname "$dest_path")"
+
+                    # Create symbolic link
+                    ln -sf "$original_file_path" "$dest_path"
                 done
             fi
         done
